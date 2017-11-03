c3213ec4c652a87788262bef1259a9625af6b303
Matteo Cellucci <matteocellucci@gmail.com>
2017-11-03 23:20:26 +0100

    Add navigator behavior

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,7 @@
 jQuery(function($) {
   // Objects
-  const fullscreen = new FullscreenController(document.documentElement);
-  const canvasesController = new CanvasesController();
   let $current = $('.diapositive.current');
   let isTransitionEnd = true;
-
-  // Init
-  $('.diapositive').each(function() {
-    const $this = $(this);
-    const canvas = canvasesController.generate($this.width(), $this.height());
-    $this.append(canvas);
-  });
 
   // Functions
   const bindTransitionEnd = function() {
@@ -22,7 +13,7 @@ jQuery(function($) {
       }
     );
   };
-  const slideLeft = function() {
+  const nextSlide = function() {
     if (isTransitionEnd) {
       const $next = $current.next();
       if ($next.length > 0) {
@@ -33,7 +24,7 @@ jQuery(function($) {
       }
     }
   };
-  const slideRight = function() {
+  const prevSlide = function() {
     if (isTransitionEnd) {
       const $prev = $current.prev();
       if ($prev.length > 0) {
@@ -44,69 +35,29 @@ jQuery(function($) {
       }
     }
   };
-  const toggleCtrl = function() {
-    const $ctrl = $('#controller');
-    if ($ctrl.hasClass('hide')) {
-      $ctrl.removeClass('hide');
-    }
-    else {
-      $ctrl.addClass('hide');
-    }
+  const toggleToolbar = function() {
+    $('#toolbar').toggleClass('show');
   };
-  const clearCanvas = function() {
-    const $currentCanvas = $current.children('.livePenCanvas');
-    CanvasesController.clearCanvas($currentCanvas[0]);
-  };
-
+  
   // Mouse event handlers
-  $('#prev-btn').on('click', slideRight);
-  $('#next-btn').on('click', slideLeft);
-  $('#fullscreen-btn').on('click', fullscreen.toggle);
-  $('#togglectrl-btn').on('click', toggleCtrl);
-  $('#clearcanvas-btn').on('click', clearCanvas);
-  $('#toolbar-toggle').on('click', e => {
-    $(this).toggleClass('show');
-  });
+  $('#menu-btn').on('click', toggleToolbar);
+  $('#prev-btn').on('click', prevSlide);
+  $('#next-btn').on('click', nextSlide);
 
   // Keyboard event handlers
   $('html').on('keydown', function(e) {
     // console.log(e.which);
     switch (e.which) {
       case 39: // right arrow
-        slideLeft();
+        nextSlide();
         break;
       case 37: // left arrow
-        slideRight();
-        break;
-      case 67: // letter c
-        clearCanvas();
-        break;
-      case 70: // letter f
-        if (fullscreen.enabled()) {
-          fullscreen.toggle();
-        }
-        break;
-      case 72: // letter h
-        toggleCtrl();
+        prevSlide();
         break;
       default:
         return;
     }
     e.preventDefault();
-  });
-
-  // Other handlers bind
-  $('#brushsize-sct').on('change', function() {
-    canvasesController.setBrushSize($(this).val());
-  });
-  $('#brushcolor-ipt').on('change', function() {
-    canvasesController.setBrushColor($(this).val());
-  });
-  $('.diapositive').on('resize', function() {
-    const $this = $(this);
-    const $canvas = $this.children('.livePenCanvas');
-    $canvas.width($this.width());
-    $canvas.height($this.height());
   });
 
 });
