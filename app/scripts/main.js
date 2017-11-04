ec83e4e592e1d154537a6da556e1344b955f04b3
Matteo Cellucci <matteocellucci@gmail.com>
2017-11-04 18:34:34 +0100

    Add keyboard controller

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,19 @@
 
   document.getElementById('prev-btn').addEventListener('click', slider.prev);
   document.getElementById('next-btn').addEventListener('click', slider.next);
+
+  window.addEventListener('keydown', e => {
+    console.log(e.key);
+    switch (e.key) {
+      case 'ArrowRight':
+        slider.next();
+        break;
+      case 'ArrowLeft':
+        slider.prev();
+        break;
+      default:
+        return;
+    }
+    e.preventDefault();
+  });
 })();
