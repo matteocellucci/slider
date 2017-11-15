644f6de7956b279606318addff4f69233accb3c7
Matteo Cellucci <matteocellucci@gmail.com>
2017-11-15 22:04:22 +0100

    Optimize the canvases controller

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,17 +7,13 @@
     const id = ids[i];
     domBuffer[id] = document.getElementById(id);
   }
-
-  const windows = {
-    canvas: new Windowable(domBuffer['canvas-window'])
-  };
-  const toolbar = new Toolbar(domBuffer['toolbar']);
   
-  const slider = new Slider('slider');
-  const canvases = new CanvasesController();
+  const slider = new Slider(domBuffer['slider']);
 
-  canvases.generate(slider.diaps);
+  const drawer = new Drawer();
+  drawer.generate(slider.diaps);
 
+  const toolbar = new Toolbar(domBuffer['toolbar']);
   toolbar.addUtility(
     'fullscreen-setting',
     document.getElementById('fullscreen-setting'),
@@ -28,33 +24,35 @@
     document.getElementById('canvas-setting'),
     () => {
       windows.canvas.toggle(); 
-      canvases.toggle();
+      drawer.toggle();
     }
   );
 
+  const windows = {
+    canvas: new Windowable(domBuffer['canvas-window'])
+  };
   windows.canvas.addButton(
     'canvas-window-brush',
     document.getElementById('canvas-window-brush'),
-    () => canvases.setBrush()
+    () => drawer.setBrush()
   );
   windows.canvas.addButton(
     'canvas-window-eraser',
     document.getElementById('canvas-window-eraser'),
-    () => canvases.setEraser()
+    () => drawer.setEraser()
   );
   windows.canvas.addButton(
     'canvas-window-clearall',
     document.getElementById('canvas-window-clearall'),
-    () => canvases.clearCanvas(slider.current)
+    () => drawer.clearCanvas(slider.current)
   );
   const sizeSettingElement = document.getElementById('canvas-window-size');
   windows.canvas.addSetting(
     'canvas-window-size',
     sizeSettingElement,
     () => {
-      const sizeIndex = sizeSettingElement.selected;
-      const size = sizeSettingElement[sizeIndex].value;
-      canvases.setSize(size);
+      const size = sizeSettingElement.value;
+      drawer.setSize(size);
     }
   );
   const colorSettingElement = document.getElementById('canvas-window-color');
@@ -63,30 +61,28 @@
     colorSettingElement,
     () => {
       const color = colorSettingElement.value;
-      canvases.setColor(color);
+      drawer.setColor(color);
     }
   );
 
-  document.getElementById('prev-btn').addEventListener('click', slider.prev);
-  document.getElementById('next-btn').addEventListener('click', slider.next);
+  document.getElementById('prev-btn').addEventListener('click', () => slider.prev());
+  document.getElementById('next-btn').addEventListener('click', () => slider.next());
   document.getElementById('menu-btn').addEventListener('click', () => toolbar.toggle());
-  window.addEventListener('resize', () => canvases.resize(document.documentElement));
+  //window.addEventListener('resize', () => canvases.resize(document.documentElement));
 
   const keys = {
     'ArrowRight': () => slider.next(),
     'ArrowLeft': () => slider.prev(),
     'f': () => toolbar.runKill('fullscreen-setting'),
-    'd': () => {
-      toolbar.runKill('canvas-setting');
-      canvases.toggle();
-    },
+    'd': () => toolbar.runKill('canvas-setting'),
     's': () => toolbar.toggle(),
-    'B': () => canvases.setBrush(),
-    'E': () => canvases.setEraser(),
-    'C': () => canvases.clearCanvas(slider.current)
+    'B': () => drawer.setBrush(),
+    'E': () => drawer.setEraser(),
+    'C': () => drawer.clearCanvas(slider.current)
   };
   for (let key in keys) {
     Keyboarder.bind(key, keys[key]);
   }
   Keyboarder.init();
+
 })();
