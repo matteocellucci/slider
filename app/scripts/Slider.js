f48f6b64d10f29bae380902c06cb5892db36474b
Matteo Cellucci <matteocellucci@gmail.com>
2017-11-15 22:05:59 +0100

    Optimize slider

--- app/scripts/Slider.js
+++ app/scripts/Slider.js
@@ -1,50 +1,40 @@
 class Slider {
-  constructor(conteinerId) {
-    Slider.transitionStatus = false;
-   
-    this.frame = document.getElementById(conteinerId);
-    this.diaps = this.frame.getElementsByClassName('diapositive');
+  constructor(element) {
+    this.domElement = element;
+    this.diaps = this.domElement.getElementsByClassName('diapositive');
     this.current = 0;
           
-    this.prev = this.prev.bind(this);
-    this.next = this.next.bind(this);
-    
-    //TransitionEnd.one(this.diaps[0], () => Slider.transitionStatus = false);
+    Slider.transitionStatus = false;
+    TransitionEnder.one(this.diaps[0], () => Slider.transitionStatus = false);
   }
 
   prev() {
-    let curr, prev, currClass, prevClass;
-    
     if (!Slider.transitionStatus && this.current > 0) {
-      curr = this.diaps[this.current];
-      prev = this.diaps[this.current - 1];
-      currClass = curr.getAttribute('class').split(/\s+current\s+/).join(" ");
-      prevClass = prev.getAttribute('class').split(/\s+prev\s+/).join(" ") + ' current ';
-      
-      TransitionEnd.one(curr, () => Slider.transitionStatus = false);
+      const curr = new Classable(this.diaps[this.current]);
+      const prev = new Classable(this.diaps[this.current - 1]);
+
+      TransitionEnder.one(this.diaps[this.current], () => Slider.transitionStatus = false);
       Slider.transitionStatus = true;
       this.current--;
       
-      curr.setAttribute('class', currClass);
-      prev.setAttribute('class', prevClass);
+      curr.removeClass('current');
+      prev.removeClass('prev');
+      prev.addClass('current');
     }
   }
 
   next() {
-    let curr, next, currClass, nextClass;
-    
     if (!Slider.transitionStatus && this.current < this.diaps.length - 1) {
-      curr = this.diaps[this.current];
-      next = this.diaps[this.current + 1];
-      currClass = curr.getAttribute('class').split(/\s+current\s+/).join(" ") + ' prev ';
-      nextClass = next.getAttribute('class') + ' current ';
+      const curr = new Classable(this.diaps[this.current]);
+      const next = new Classable(this.diaps[this.current + 1]);
       
-      TransitionEnd.one(curr, () => Slider.transitionStatus = false);
+      TransitionEnder.one(this.diaps[this.current], () => Slider.transitionStatus = false);
       Slider.transitionStatus = true;
       this.current++;
-      
-      curr.setAttribute('class', currClass);
-      next.setAttribute('class', nextClass);
+     
+      curr.removeClass('current');
+      curr.addClass('prev');
+      next.addClass('current');
     }
   }
 }
