49a013d6431f7de11f0fdfba795f3281bcecc486
Matteo Cellucci <matteocellucci@gmail.com>
2017-11-22 22:54:10 +0100

    Init viewer test unit

--- app/scripts/core/Viewer.js
+++ app/scripts/core/Viewer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let Viewer = (state) => {
+
   return {
     showUi() {},
     hideUi() {},
--- test/spec/Viewer-test.js
+++ test/spec/Viewer-test.js
@@ -4,27 +4,24 @@
   describe('Viewer', function () {
     describe('when adding viewer behavior', function () {
       it('should add showUi behavior', function () {
-        const viewer = Object.assign(
-          {},
-          Viewer({})
-        );
+        const viewer = Object.assign({}, Viewer());
         expect(viewer.showUi).to.exist;
       });
       
       it('should add hideUi behavior', function () {
-        const viewer = Object.assign(
-          {},
-          Viewer({})
-        );
+        const viewer = Object.assign({}, Viewer());
         expect(viewer.hideUi).to.exist;
       });
       
       it('should add toggleUi behavior', function () {
-        const viewer = Object.assign(
-          {},
-          Viewer({})
-        );
+        const viewer = Object.assign({}, Viewer());
         expect(viewer.toggleUi).to.exist;
+      });
+      
+      it('should extend the object and do not transform it', function () {
+        const state = document.createElement('test');
+        const viewer = Object.assign(state, Viewer(state));
+        expect(viewer.tagName).to.equal('TEST');
       });
     });
     
